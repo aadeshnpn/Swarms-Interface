363ff465a7136ce8e2a6d623567b87c79c5d1702
Jonah <jsh229.wa@gmail.com>
2016-11-12 20:51:19 -0700

    remove leftover stuff from inline server sim, add IPC, refactor gui stuff to be completely independent of the server

--- viewer/index.js
+++ viewer/index.js
@@ -15,12 +15,7 @@ var io = require( 'socket.io' )( http );
 // The server's job should be essentially to grab world info from the simulation
 // engine and pass it on to the client, nothing else.
 
-//var worlds = [];
 var numClients = 0;
-
-// This will be used as a hash of selected agents by client id
-// e.g. clientSelected[clientId][agentId] = true if selected, unset otherwise
-var clientSelected = [];
 
 /*******************************************************************************
  * IPC setup
@@ -44,41 +39,11 @@ ipc.server.on('start', function()
 {
    // We expect data in JSON according to the following format:
    // '{ type: "update", data: <world object> }'
-   console.log('started');
+
    ipc.server.on('update', function(world, client)
    {
-      // Loop through each connected client so we can add a 'selected' tag
-      // onto their agents
-      /*for (var socketId in io.sockets.connected)
-      {
-         var socket = io.sockets.connected[socketId];
-
-         // NOTE: this is a reference, not a deep copy,
-         //       so unless we change that we have to make
-         //       sure agent ui props are properly reset on each pass
-         var agents = world.agents;
-
-         for (var i = 0; i < agents.length; i++)
-         {
-            var agent = agents[i];
-
-            if (clientSelected[socket.id] && clientSelected[socket.id][agent.id] == true)
-            {
-               // TODO: hide ui stuff under a agent.ui object?
-               agent.selected = true;
-            }
-            else
-            {
-               // avoid other clients getting the wrong agents selected
-               agent.selected = false;
-            }
-
-            socket.emit("update", world);
-         }*/
-
-         io.sockets.emit("update", world);
-         ipc.server.emit(client, 'received');
-      //};
+      io.sockets.emit("update", world);
+      ipc.server.emit(client, 'received');
    });
 });
 
@@ -118,139 +83,12 @@ http.listen( process.env.PORT || 3000, function()
 // object, not the global io object
 io.on( 'connection', function( socket )
 {
-   //worlds[ socket.id ] = newWorld();
-
-   /*var closure = function()
-   {
-      update( socket );
-   }
-
-   var interval = setInterval( closure, 16 );*/
-
    numClients++;
-   console.log("Client " + socket.id + " connected. (" + numClients + " total)");
+   console.log("Client " + socket.id + " connected.    (" + numClients + " total)");
 
    socket.on( 'disconnect', function()
    {
-      //clearInterval( interval );
-      //worlds[ socket.id ] = null;
       numClients--;
       console.log("Client " + socket.id + " disconnected. (" + numClients + " total)");
    } );
-
-   // Pure UI functionality like this (selecting/unselecting) should be handled
-   // here in the server (i.e., the engine shouldn't know or care about it).
-   // We'll need something similar to what I have below keeping track of which
-   // entities have been selected so we can mark them as such on new updates
-   // before sending it out to the client
-
-   // expects an array of selected ids
-   socket.on( 'select', function( ids )
-   {
-      clientSelected[socket.id] = ids;
-   } );
-
-   /*socket.on( 'pause', function( ids )
-   {
-      var agents = worlds[socket.id].agents;
-
-      if (ids == -1)
-      {
-         for (var i = 0; i < agents.length; i++)
-         {
-            agents[i].state = "paused";
-         }
-      }
-      else
-      {
-         for (var i = 0; i < ids.length; i++)
-         {
-            worlds[ socket.id ].agents[ ids[i] ].state = "paused";
-         }
-      }
-   } );
-
-   socket.on( 'resume', function( ids )
-   {
-      var agents = worlds[socket.id].agents;
-
-      if (ids == -1)
-      {
-         for (var i = 0; i < agents.length; i++)
-         {
-            agents[i].state = "normal";
-         }
-      }
-      else
-      {
-         for (var i = 0; i < ids.length; i++)
-         {
-            worlds[ socket.id ].agents[ ids[i] ].state = "normal";
-         }
-      }
-   } );*/
-
-   socket.on( 'deselect', function()
-   {
-      clientSelected[socket.id] = [];
-   } );
-
 } );
-
-/*******************************************************************************
- * Leftover from the default sim
- *******************************************************************************
-
-/*function update( socket )
-{
-   var world = worlds[ socket.id ];
-
-   for ( var i = 0; i < world.agents.length; i++ )
-   {
-      var agent = world.agents[ i ];
-
-      if (agent.state == "paused")
-      {
-         continue;
-      }
-
-      agent.x += agent.vX;
-      agent.y += agent.vY;
-
-      if ( agent.x <= 0 || agent.x >= world.width )
-      {
-         agent.vX *= -1;
-      }
-      if ( agent.y <= 0 || agent.y >= world.height )
-      {
-         agent.vY *= -1;
-      }
-   }
-
-   socket.emit( "update", world );
-}*/
-
-/*function newWorld()
-{
-   world = {
-      "width": 500,
-      "height": 500,
-      "agents": []
-   };
-
-   for ( var i = 0; i < ( Math.round( Math.random() * 80 ) + 20 ); i++ )
-   //for ( var i = 0; i < 1000; i++ )
-   {
-      world.agents[ i ] = {
-         id: i,
-         x: Math.round( Math.random() * world.width ),
-         y: Math.round( Math.random() * world.height ),
-         vX: Math.round( Math.random() * 4 ) + 1,
-         vY: Math.round( Math.random() * 4 ) + 1,
-         selected: false,
-         state: "normal"
-      }
-   }
-
-   return world;
-}*/
