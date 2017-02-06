f935e2c5a2af458925954598733d6622fca26c99
joshmccl <joshuasmcclellan@gmail.com>
2017-02-06 14:20:17 -0700

    added communication with the interface

--- environment_code/Environment.py
+++ environment_code/Environment.py
@@ -537,6 +537,10 @@ class Environment:
                     "attractors": list(map(lambda a: a.toJson(), self.attractors)),
                     "repulsors" : list(map(lambda r: r.toJson(), self.repulsors )), #self.repulsors,
                     "agents"    : self.agents_to_json()
+                },
+                "hub":
+                {
+                        "agentDirections" : self.hubController.directions
                 }
             })
         )
